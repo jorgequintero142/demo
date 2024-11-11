bc597f581fe2881408e57e67be5bd075f5fffd59
Jorge Quintero <jorgequintero142@gmail.com>
2024-11-10 21:09:10 -0500

    gitignore and select language

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
-    final static String HOLA = "Hola mundo";
+    final static String HOLA_ES = "Hola mundo";
+    final static String HOLA_EN = "Hello world";
 
     public static void main(String[] args) {
-        System.out.println(Main.HOLA);
+        if (args.length>0 && args[0].equals("en")) 
+            System.out.println(Main.HOLA_EN+"<>"+args[0]);
+        else
+            System.out.println(Main.HOLA_ES);
     }
 }
