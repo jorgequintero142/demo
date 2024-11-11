6fc3f0a68e1989b7ecdd83801c7fc39354060bc0
Jorge Quintero <jorgequintero142@gmail.com>
2024-11-10 21:22:05 -0500

    other message

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ public class Main {
     final static String HOLA_EN = "Hello world";
 
     public static void main(String[] args) {
-        if (args.length>0 && args[0].equals("en")) 
-            System.out.println(Main.HOLA_EN+"<>"+args[0]);
+        System.out.println("Welcome back Jorge...");
+        if (args.length > 0 && args[0].equals("en"))
+            System.out.println(Main.HOLA_EN + "<>" + args[0]);
         else
             System.out.println(Main.HOLA_ES);
     }
