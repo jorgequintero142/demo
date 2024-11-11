3d789b644eab4e330c7948e5edf0cf47a8a186a3
Jorge Quintero <jorgequintero142@gmail.com>
2024-11-10 20:42:25 -0500

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    final static String HOLA = "Hola mundo";
+
     public static void main(String[] args) {
-            System.out.println("Hola mundo...");
+        System.out.println(Main.HOLA);
     }
 }
